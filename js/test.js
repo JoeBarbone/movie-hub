dd62b102fc4d341d1dedb6473aed85cfdeb0c4e8
Joseph Barbone <JoeBarbone@aol.com>
2022-05-30 16:32:09 -0400

    test.js file to test layout

---
+++ js/test.js
@@ -0,0 +1,33 @@
+// This is a file that contains test data
+
+//#ratings
+
+document.getElementById("ratings").innerHTML = "<p class='title is-1'>Movie Information</p>" + 
+                 "<span class='pl-3 has-text-weight-bold'>Title:</span> " + "Switch" + "<br />" + 
+                 "<span class='pl-3 has-text-weight-bold'>IMDB ID:</span> " + "tt012968" + "<br /><br /><br />" +
+                 "<p class='subtitle is-3'>Critic Ratings</p>" + 
+                 "<strong class='pl-3'><a href='https://www.imdb.com/title/" + "tt012968" + 
+                 "' target='_blank'>" + "IMDB Rating:</strong> " + "6.8" + "/10</a><br />" + 
+                 "<strong class='pl-3'>Spoiled Potatoes Rating:</strong> " + "68" + "/100 <br />" + 
+                 "<strong class='pl-3'>The Movie DB Rating:</strong> " + "6.8" + "/10 <br /><br /><br />";
+
+
+
+document.getElementById("age-group").innerHTML = "Age Group<br /><br />" + "Under 18<br />" + "18 to 29<br />" + "30 to 44<br />" + "Over 45<br /><br />" + "Overall";
+
+
+document.getElementById("user-rating").innerHTML = "User Rating<br /><br />" + 
+                "6.0/10" + "<br />" +  
+                "6.8/10" + "<br />" + 
+                "5.7/10" + "<br />" + 
+                "6.5/10"  + "<br /><br />" + 
+                "6.0/10";
+
+
+document.getElementById("votes").innerHTML = "1" + "<br />" +
+    "294" + "<br />" +
+    "3426" + "<br />" +
+    "9600" + "<br /><br />" +
+    "10322";
+
+                                                
